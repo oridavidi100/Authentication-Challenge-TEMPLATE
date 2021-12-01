02436e4ecd0951a1aff90585698653f16a7902a9
ori davidi <oridavidi100@gmail.com>
2021-12-01 16:48:06 +0200

    end of tests!

--- backEnd/controlers/controoller.js
+++ backEnd/controlers/controoller.js
@@ -1,15 +1,65 @@
-const USERS = []; // [...{email, name, password, isAdmin}...]
-const INFORMATION = []; // [...{email, info}...]
-// const REFRESHTOKENS = [];
+const USERS = [
+  {
+    email: 'admin@email.com',
+    name: 'admin',
+    password: '$2b$10$DHvaY7RMYSkqOpJWdQ6qn.k5NNkdcja8p0OcRP0rLzObYhsonacvG',
+    isAdmin: true,
+  },
+];
+
+const serverApis = [
+  {
+    method: 'post',
+    path: '/users/register',
+    description: 'Register, Required: email, name, password',
+    example: { body: { email: 'user@email.com', name: 'user', password: 'password' } },
+  },
+  {
+    method: 'post',
+    path: '/users/login',
+    description: 'Login, Required: valid email and password',
+    example: { body: { email: 'user@email.com', password: 'password' } },
+  },
+  {
+    method: 'post',
+    path: '/users/token',
+    description: 'Renew access token, Required: valid refresh token',
+    example: { headers: { token: 'Refresh Token' } },
+  },
+  {
+    method: 'post',
+    path: '/users/tokenValidate',
+    description: 'Access Token Validation, Required: valid access token',
+    example: { headers: { Authorization: 'Bearer Access Token' } },
+  },
+  {
+    method: 'get',
+    path: '/api/v1/information',
+    description: "Access user's information, Required: valid access token",
+    example: { headers: { Authorization: 'Bearer Access Token' } },
+  },
+  {
+    method: 'post',
+    path: '/users/logout',
+    description: 'Logout, Required: access token',
+    example: { body: { token: 'Refresh Token' } },
+  },
+  {
+    method: 'get',
+    path: 'api/v1/users',
+    description: 'Get users DB, Required: Valid access token of admin user',
+    example: { headers: { authorization: 'Bearer Access Token' } },
+  },
+];
+const INFORMATION = [{ email: 'admin@email.com', name: 'admin info' }]; // [...{email, info}...]
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const SECRET = process.env.ACCESS_TOKEN_SECRET;
-const REFRESHTOKENS = [];
+let REFRESHTOKENS = [];
 const bcrypt = require('bcrypt');
-// const salt = 10;
 
 exports.register = async (req, res, next) => {
-  const { email, user } = req.body;
+  const { email, name } = req.body;
   let { password } = req.body;
   let isAdmin = false;
   if (password === 'Rc123456!') {
@@ -22,10 +72,9 @@ exports.register = async (req, res, next) => {
       return res.status(409).send('user already exists');
     }
   }
-  USERS.push({ email: email, name: user, password: password, isAdmin: isAdmin });
-  INFORMATION.push({ email: email, info: `${user} info` });
+  USERS.push({ email: email, name: name, password: password, isAdmin: isAdmin });
+  INFORMATION.push({ email: email, info: `${name} info` });
   res.status(201).send('Register Success');
-  console.log(USERS, INFORMATION);
   next();
 };
 
@@ -35,15 +84,14 @@ exports.login = async (req, res, next) => {
   let flag = 0;
   for (let user1 of USERS) {
     let ans = await bcrypt.compare(password, user1.password);
-    console.log(ans);
     if (email === user1.email && ans) {
       body.email = user1.email;
       body.name = user1.name;
       body.isAdmin = user1.isAdmin;
-      username = { email: email, password: password };
-      const accessToken = jwt.sign(username, SECRET, { expiresIn: '70s' });
+      username = { email: email, name: body.name, isAdmin: body.isAdmin };
+      const accessToken = jwt.sign(username, SECRET, { expiresIn: '10s' });
       body.accessToken = accessToken;
-      body.refreshToken = jwt.sign(username, process.env.REFRESH_TOKEN_SECRET);
+      body.refreshToken = jwt.sign({ username: username, isAdmin: body.isAdmin }, process.env.REFRESH_TOKEN_SECRET);
       if (!REFRESHTOKENS.includes(body.refreshToken)) {
         REFRESHTOKENS.push(body.refreshToken);
       }
@@ -62,9 +110,8 @@ exports.login = async (req, res, next) => {
   next();
 };
 
-exports.authentication = (req, res, next) => {
+exports.tokenValidate = (req, res, next) => {
   const authHeader = req.headers['authorization'];
-  console.log(authHeader);
   const token = authHeader && authHeader.split(' ')[1];
   if (token === null) {
     return res.status(401).send('Access Token Required');
@@ -73,6 +120,7 @@ exports.authentication = (req, res, next) => {
     if (err) {
       return res.status(403).send('Invalid Access Token');
     }
+
     const body = { valid: true };
     res.status(200).send(body);
     next();
@@ -80,7 +128,6 @@ exports.authentication = (req, res, next) => {
 };
 exports.information = (req, res, next) => {
   const authHeader = req.headers['authorization'];
-  console.log(authHeader);
   const token = authHeader && authHeader.split(' ')[1];
   if (token === null) {
     return res.status(401).send('Access Token Required');
@@ -96,14 +143,14 @@ exports.information = (req, res, next) => {
         body.info = info.info;
       }
     }
-    res.status(200).send(body);
+    res.status(200).send([body]);
     next();
   });
 };
 
 exports.token = (req, res, next) => {
   const refreshToken = req.body.token;
-  if (refreshToken === null) {
+  if (!refreshToken) {
     return res.status(401).send('Refresh Token Required');
   }
   if (!REFRESHTOKENS.includes(refreshToken)) {
@@ -113,8 +160,7 @@ exports.token = (req, res, next) => {
     if (err) {
       return res.status(403).send('Invalid Refresh Token');
     }
-    console.log(user);
-    const accessToken = jwt.sign({ email: user.email }, SECRET, { expiresIn: '5s' });
+    const accessToken = jwt.sign({ username: user.username, isAdmin: user.isAdmin }, SECRET, { expiresIn: '10s' });
     const body = {};
     body.accessToken = accessToken;
     res.status(200).send(body);
@@ -123,38 +169,63 @@ exports.token = (req, res, next) => {
 };
 
 exports.logout = (req, res, next) => {
-  if (req.body.token === nul) {
-    return res.status(400).send('Refresh Token Required');
+  try {
+    if (req.body.token === null) {
+      return res.status(400).send('Refresh Token Required');
+    }
+    if (!REFRESHTOKENS.includes(req.body.token)) {
+      return res.status(400).send('Invalid Refresh Token');
+    }
+    REFRESHTOKENS = REFRESHTOKENS.filter((token) => {
+      token !== req.body.token;
+    });
+    res.status(200).send('User Logged Out Successfully');
+  } catch (err) {
+    console.log(err);
   }
-  if (!REFRESHTOKENS.includes(req.body.token)) {
-    return res.status(400).send('Invalid Refresh Token');
-  }
-  REFRESHTOKENS = REFRESHTOKENS.filter((token) => {
-    token !== req.body.token;
-  });
-  res.status(200).send('User Logged Out Successfully');
 };
 
 exports.users = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  if (token === null) {
-    return res.status(401).send('Access Token Required');
-  }
-  jwt.verify(token, SECRET, (err, user) => {
-    if (err) {
-      return res.status(403).send('Invalid Access Token');
+  try {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token === null) {
+      return res.status(401).send('Access Token Required');
     }
-
-    const body = {};
-    for (let user1 of USERS) {
-      if (user1.email === user.email) {
-        if (user1.isAdmin) {
-          body.users = USERS;
-          return res.status(200).send(body);
+    jwt.verify(token, SECRET, (err, user) => {
+      if (err) {
+        return res.status(403).send('Invalid Access Token');
+      }
+      for (let user1 of USERS) {
+        if (user1.email === user.email) {
+          if (user1.isAdmin) {
+            return res.status(200).send(USERS);
+          }
         }
       }
+      return res.status(403).send('Invalid Access Token');
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.options = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) {
+    return res.header({ Allow: 'OPTIONS, GET, POST' }).send([serverApis[0], serverApis[1]]);
+  }
+  jwt.verify(token, SECRET, (err, user) => {
+    console.log(user);
+    if (err) {
+      return res.header({ Allow: 'OPTIONS, GET, POST' }).send([serverApis[0], serverApis[1], serverApis[2]]);
     }
-    next();
+    if (user.isAdmin) {
+      return res.header({ Allow: 'OPTIONS, GET, POST' }).send(serverApis);
+    }
+    return res
+      .header({ Allow: 'OPTIONS, GET, POST' })
+      .send([serverApis[0], serverApis[1], serverApis[2], serverApis[3], serverApis[4], serverApis[5]]);
   });
 };
--- backEnd/routes/apirouter.js
+++ backEnd/routes/apirouter.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, authentication, information, token, logout, users } = require('../controlers/controoller');
+const jwt = require('jsonwebtoken');
+
+const {
+  register,
+  login,
+  tokenValidate,
+  information,
+  token,
+  logout,
+  users,
+  options,
+} = require('../controlers/controoller');
 
 //requset
 router.post('/users/login', login);
 router.post('/users/register', register);
-router.post('/users/tokenValidate', authentication);
+router.post('/users/tokenValidate', tokenValidate);
 router.get('/api/v1/information', information);
 router.post('/users/token', token);
 router.post('/users/logout', logout);
 router.get('/api/v1/users', users);
-router.options('/', (req, res, next) => {});
+router.options('/', options);
 module.exports = router;
